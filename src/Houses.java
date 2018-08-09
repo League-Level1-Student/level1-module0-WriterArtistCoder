c19359ef041cd9267820b0d0ec8e6f5b1ea3bca6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-09 14:54:02 -0700

    Day 4

--- src/Houses.java
+++ src/Houses.java
@@ -1,38 +1,54 @@
+import java.util.Random;
 import org.jointheleague.graphical.robot.*;
 public class Houses {
 	public static void main(String[] args) {
 		Robot bob = new Robot(); // As in Bob the Builder
+		String[] heights = {"small", "small", "small", "medium", "medium", "medium", "large", "large", "large"};
 		bob.miniaturize();
 		bob.moveTo(0, 500);
-		for (int i = 0; i < 10; i++) {
-			drawHouse(bob, 100, false);
+		for (int i = 0; i < 9; i++) {
+			int randR = new Random().nextInt(256);
+			int randB = new Random().nextInt(256);
+			drawHouse(bob, randR, 0, randB, heights[i]);
 		}
 	}
 	
-	public static void drawHouse(Robot bot, int height, boolean point) {
+	public static void drawHouse(Robot bot, int r, int g, int b, String heightStr) {
 		// Setup
 		bot.setSpeed(500);
 		bot.penDown();
+		int height;
+		boolean point = true;
+		if (heightStr.equalsIgnoreCase("small")) {
+			height = 60;
+		} else if (heightStr.equalsIgnoreCase("medium")) {
+			height = 120;
+		} else if (heightStr.equalsIgnoreCase("large")) {
+			height = 250;
+			point = false;
+		} else {
+			height = 0;
+		}
 		// Draw grass
 		bot.setPenColor(0, 100, 0);
 		bot.turn(90);
-		bot.move(50);
+		bot.move(20);
 		// Draw house
-		bot.setRandomPenColor();
+		bot.setPenColor(r, g, b);
 		bot.turn(-90); // Build left wall
 		bot.move(height);
 		
 		if (point) {
 			bot.turn(45); // Build left roof
-			bot.move(50);
+			bot.move(25);
 		
 			bot.turn(90); // Build right roof
-			bot.move(50);
+			bot.move(25);
 		
 			bot.turn(45); // Build right wall
 		} else {
 			bot.turn(90);
-			bot.move(80);
+			bot.move(60);
 			bot.turn(90);
 		}
 		bot.move(height);
